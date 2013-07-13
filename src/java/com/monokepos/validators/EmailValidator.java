56137468140cbc6a9f05d651540fb4a8b387ec34
jaime-rey <jaimereycasado@gmail.com>
2013-07-13 23:37:23 +0200

    recién añadido el validator

---
+++ src/java/com/monokepos/validators/EmailValidator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.monokepos.validators;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author jaime
+ */
+public class EmailValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
